404aea2f0d5e9affcf5c898d050b321edac62d50
KC-2001MS <79145483+KC-2001MS@users.noreply.github.com>
2022-04-13 00:18:35 +0900

    Initial Commit

---
+++ Shared/testaudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  testaudioApp.swift
+//  Shared
+//
+//  Created by 茅根啓介 on 2022/04/13.
+//
+
+import SwiftUI
+
+@main
+struct testaudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
