f779da17e46c95692c28051bee14fc5b66664208
KC-2001MS <79145483+KC-2001MS@users.noreply.github.com>
2022-04-15 21:50:00 +0900

    すみません。これです。

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,11 +6,36 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+            let synthesizer = AVSpeechSynthesizer()
+            let utterance = AVSpeechUtterance(string: "test 123")
+            utterance.voice = AVSpeechSynthesisVoice(language: "en")
+            var output: AVAudioFile?
+
+            synthesizer.write(utterance) { (buffer: AVAudioBuffer) in
+               guard let pcmBuffer = buffer as? AVAudioPCMBuffer else {
+                  fatalError("unknown buffer type: \(buffer)")
+               }
+               if pcmBuffer.frameLength == 0 {
+                 // done
+               } else {
+                 // append buffer to file
+                 if output == nil {
+                   output = try? AVAudioFile(
+                     forWriting: URL(fileURLWithPath: NSHomeDirectory() + "test.caf"),
+                     settings: pcmBuffer.format.settings,
+                     commonFormat: .pcmFormatInt16,
+                     interleaved: false)
+                 }
+                   try? output?.write(from: pcmBuffer)
+               }
+            }
+        }
     }
 }
 
