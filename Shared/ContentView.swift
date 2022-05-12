fc3e5b1b8b568b51c0bb7e4e7fc6ecba98900f07
KC-2001MS <79145483+KC-2001MS@users.noreply.github.com>
2022-05-12 13:41:04 +0900

    コードレベルサポートの内容の反映

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,25 +9,30 @@ import SwiftUI
 import AVFoundation
 
 struct ContentView: View {
+    let synthesizer = AVSpeechSynthesizer()
     var body: some View {
         Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
-            let synthesizer = AVSpeechSynthesizer()
             let utterance = AVSpeechUtterance(string: "test 123")
             utterance.voice = AVSpeechSynthesisVoice(language: "en")
             var output: AVAudioFile?
 
             synthesizer.write(utterance) { (buffer: AVAudioBuffer) in
+                print("closure called")
+                guard let dirURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
+                fatalError("フォルダURL取得エラー")
+                }
+                
+                let fileURL = dirURL.appendingPathComponent("test.caf")
                guard let pcmBuffer = buffer as? AVAudioPCMBuffer else {
                   fatalError("unknown buffer type: \(buffer)")
                }
                if pcmBuffer.frameLength == 0 {
                } else {
                  if output == nil {
+                     let bufferSettings = utterance.voice?.audioFileSettings
                    output = try? AVAudioFile(
-                     forWriting: URL(fileURLWithPath: NSHomeDirectory() + "/Docments/test.caf"),
-                     settings: pcmBuffer.format.settings,
-                     commonFormat: .pcmFormatInt16,
-                     interleaved: false)
+                     forWriting: fileURL,
+                     settings: bufferSettings!)
                  }
                    try? output?.write(from: pcmBuffer)
                }
